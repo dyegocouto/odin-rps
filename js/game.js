bab7bce20349aa6f194136f022214de7c3cb9dca
dyegocouto <175648963+dyegocouto@users.noreply.github.com>
2025-09-06 09:41:12 -0300

    Implement getHumanChoice()
    Function asks for a move of rps. Validation process is simple for now.

--- js/game.js
+++ js/game.js
@@ -3,3 +3,14 @@ const CHOICES = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
+
+function getHumanChoice() {
+  let humanChoice = "";
+  do {
+    humanChoice = prompt(
+      "Please, choose: rock, paper, or scissors?"
+    ).toLowerCase();
+  } while (!CHOICES.includes(humanChoice));
+
+  return humanChoice;
+}
